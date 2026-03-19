d8c3a182bf20dbf18a2c6916c4fde646d7494444
Emily Ning <eningfu@gmail.com>
2026-03-19 15:19:35 +1100

    make responsive

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,13 +1,15 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
+import gd from "../assets/gallery-gd.png";
+
 function Gallery() {
   return (
     <>
       <Navbar />
       <div className="container">
         <div className="gallery">
-
+          <img src={gd} alt="Posters" width="40%" />
         </div>
       </div>
       <Footer />
