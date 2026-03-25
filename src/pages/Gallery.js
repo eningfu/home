f8fe387d6ae5176f1049602d5ac59190c86cd77c
Emily Ning <eningfu@gmail.com>
2026-03-25 15:01:32 +1100

    add video

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,6 +2,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 import gd from "../assets/gallery-gd.png";
+import mg from "../assets/nalina.mp4";
 
 function Gallery() {
   return (
@@ -10,6 +11,10 @@ function Gallery() {
       <div className="container">
         <div className="gallery">
           <img src={gd} alt="Posters" width="40%" />
+          <video width="100%" controls>
+            <source src={mg} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
       <Footer />
